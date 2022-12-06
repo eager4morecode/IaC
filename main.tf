82d372b84527055ad282288594a0d47a31158822
eager4morecode <115359627+eager4morecode@users.noreply.github.com>
2022-12-06 13:31:17 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "stsdemowk3_ec2" {
   ami = "ami-0b0dcb5067f052a63"
   instance_type = "t2.micro"
   key_name = "wk3demo"
-  vpc_security_groups = [aws_security_group.dynamicsg-sts.id]
+  security_groups = ["dynamicsg-sts"]
   subnet_id = "subnet-0bdaaaec1588e5a40"
   tags = {
    Name = "EC2"
@@ -16,5 +16,3 @@ resource "aws_instance" "stsdemowk3_ec2" {
 }
   data "local_file" "user_data" {
     filename = "${path.module}/user_data.sh"
-}
-
